7afcbc845b03e5f07c16a1054b88f94a4c93d277
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2022-03-17 20:43:16 +0100

    Add function to plot history

---
+++ lib/plot_learning_curves.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+def plot_history(history, 
+                 bodypart,
+                 name_model,
+                 epochs,
+                 data_augmentation='noaugment'):
+
+  fig1  = plt.figure(figsize=(8,4), dpi = 150)
+
+  # summarize history for accuracy
+  plt.plot(history.history['binary_accuracy'], c = 'm')
+  plt.plot(history.history['val_binary_accuracy'], "--", c = 'c')
+  plt.title('Model accuracy for CNN: ' + bodypart, fontsize=16)
+  plt.ylabel('accuracy', fontsize=16)
+  plt.xlabel('epoch', fontsize=16)
+  plt.legend(['train', 'validation'], 
+             loc='upper right', 
+             fontsize=16)
+  matplotlib.rc('xtick', labelsize=16) 
+  matplotlib.rc('ytick', labelsize=16) 
+
+  plt.show()
+  plt.savefig('data/plots/accuracy_' + name_model + "_" + str(epochs) + "epochs" + data_augmentation + bodypart + '.png')
+  plt.clf()
+    
+  fig2  = plt.figure(figsize=(8,4), dpi = 150)
+
+  # summarize history for loss
+  plt.plot(history.history['loss'], c = 'm')
+  plt.plot(history.history['val_loss'], "--",  c = 'c')
+  plt.title('Model loss for CNN: ' + bodypart, fontsize=16)
+  plt.ylabel('loss', fontsize=16)
+  plt.xlabel('epoch', fontsize=16)
+  plt.legend(['train', 'validation'], 
+             loc='upper right', 
+             fontsize=16)
+  matplotlib.rc('xtick', labelsize=16) 
+  matplotlib.rc('ytick', labelsize=16) 
+  plt.show()
+  plt.savefig('data/plots/loss' + name_model + "_" + str(epochs) + "epochs" + data_augmentation + bodypart + '.png')
+  plt.clf()
