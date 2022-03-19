ed2aa45069ae0215a9f45c6bbaf5264503256140
aandrovitsanea <anna.androvitsanea@th-luebeck.de>
2022-03-19 09:23:03 +0100

    Old and new

--- lib/fine_tune.py
+++ lib/fine_tune.py
@@ -47,8 +47,12 @@ from sklearn.metrics import classification_report
 # from google.colab import drive
 print("Tensorflow version " + tf.__version__)
 
-def parameters_for_tuning():
-    
+
+
+
+
+def model_builder(hp, conv_dropout=True):
+
     #########################################
     ## set hyperparameters for fine tuning ##
     #########################################
@@ -59,201 +63,191 @@ def parameters_for_tuning():
                                step=32)
     
     # tune the learning rate for the optimizer
-    hp_learning_rate = hp.Choice('learning_rate', values=[min_value=1e-4,
-                                                          max_value=1e-2, 
-                                                          sampling="log"])
+    hp_learning_rate = hp.Choice('learning_rate', 
+                                 values=[1e-3, 1e-2, 1e-4])
     optimizer = keras.optimizers.Adam(learning_rate=hp_learning_rate)
 
     # convulational layers
-    conv_layers = hp.Int('conv_blocks', 2, 5, default=3)
+    conv_layers = hp.Int('conv_blocks', 2, 6, 
+                         default=3)
 
     # dropout rate
-    dropout_rate = hp.Float('dropout', 0.1, 0.5, step=0.1, default=0.2)
+    dropout_rate = hp.Float('dropout', 0, 0.5, 
+                            step=0.1, default=0.2)
+#     # activation
+#     activation=hp.Choice("activation", ["relu", "tanh"])
     
-    # activation
-    activation=hp.Choice("activation", ["relu", "tanh"])
-    
-    # filters
-    filters = hp.Int('filters_' + str(i), 32, 224, step=32)
-    
-    return hp_units, optimizer, conv_layers, dropout_rate, activation, filters
-    
-def build_model(input_shape,
-                conv_layers,
-                conv_activation,
-                conv_dropout,
-                units,
-                output_activation,
-                optimizer,
-                loss,
-                metrics,
-                verbose=1):
-    
-    np.random.seed(1234) # Define the seed for numpy to have reproducible experiments.
-    set_seed(9876) # Define the seed for Tensorflow to have reproducible experiments.
-    
-    # Define the input layer.
+    np.random.seed(1000) # Define the seed for numpy to have reproducible experiments.
+    set_seed(150) # Define the seed for Tensorflow to have reproducible experiments.
+
+    ###################
+    ## define model ##
+    ###################
+
+    # define the input layer
     input = Input(
         shape=input_shape,
         name='Input'
     )
 
     x = input
-    # Define the convolutional layers.
+    # define the convolutional layers
+    # using functional api
     for i in range(conv_layers):
+        # filters
+        filters = hp.Int('filters_' + str(i), 32, 224, 
+                         step=32)
+
         x = Conv2D(
             filters=filters,
             kernel_size=(3, 3),
             strides=(1, 1),
             padding='same',
             dilation_rate=(1, 1),
-            activation=activation,
+            activation="relu",
             name='Conv2D-{0:d}'.format(i + 1)
         )(x)
+
         x = MaxPool2D(
             pool_size=(2, 2),
             strides=(2, 2),
             padding='same',
             name='MaxPool2D-{0:d}'.format(i + 1)
         )(x)
-        x = Dropout(
-            rate=dropout_rate,
-            name='Dropout-{0:d}'.format(i + 1)
-        )(x)
-    # Flatten the convolved images so as to input them to a Dense Layer
+
+        if conv_dropout:
+            x = Dropout(
+                rate = dropout_rate,
+                name='Dropout-{0:d}'.format(i + 1)
+            )(x)
+
+    # flatten the convolved images 
+    # fit to feed the dense layer
     x = Flatten(name='Flatten')(x)
     
     # Add a dense before output layer and drop out
     x = tf.keras.layers.Dense(units=hp_units,
-                              activation=activation,
+                              activation="relu",
                               name='Dense-1')(x)
 
     x = tf.keras.layers.Dropout(rate=dropout_rate, 
                                 name='Dropout-{0:d}'.format(conv_layers + 1))(x)
-    # Define the output layer.
+    
+    
+    # define the output layer
     output = Dense(
-        units=units,
-        activation=output_activation,
+        units = 1,
+        activation = 'softmax',
         name='Output'
     )(x)
 
-    # Define the model and train it.
-    model = Model(inputs=input, 
-                  outputs=output)
-    model.compile(optimizer=optimizer, 
-                  loss=loss, 
-                  metrics=metrics)
-    
-    # print summary and architecture
-    model.summary()
+    # define the model and train it
+    model = Model(inputs=input, outputs=output)
 
+    model.compile(
+                  optimizer=optimizer, 
+                  loss='binary_crossentropy', 
+                  metrics=['accuracy'])
+    
     return model
-
-def tuner_hyperband(model, 
-                    objective, # 'val_accuracy'
-                    max_epochs,
-                    hyperband_iterations, # 2
-                    directory=directory, #where to save the weight
-                    project_name=project_name
-                    ):
-    # build tuner to fine-tune the hyper-parameters
-    # for the model
+    
+def tuner_search(directory,
+                 project_name,
+                 max_epochs,
+                 hyperband_iterations):
+    print(bodypart)
+    
     tuner = kt.Hyperband(
-                        model,
-                        objective=objective,
-                        max_epochs=max_epochs,
-                        hyperband_iterations=hyperband_iterations,
-                        directory=directory,
-                        project_name=project_name
-    )
-    
-    return tuner
+                    model_builder,
+                    objective='val_accuracy',
+                    max_epochs=max_epochs,
+                    hyperband_iterations=hyperband_iterations,
+                    directory=directory,
+                    project_name=project_name,
+                    patience,
+                    bodypart,
+                    epochs
+                    )
 
-def tuner_search(model, 
-                objective, # 'val_accuracy'
-                max_epochs,
-                hyperband_iterations, # 2
-                directory, #where to save the weight
-                project_name,
-                monitor, # 'val_loss'
-                patience,
-                epochs):
-    
-    tuner = tuner_Hyperband(model, 
-            objective, # 'val_accuracy'
-            max_epochs,
-            hyperband_iterations, # 2
-            directory=directory, #where to save the weight
-            project_name=project_name
-            )
     # define the early stopping
-    stop_early = tf.keras.callbacks.EarlyStopping(monitor=monitor, 
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_binary_accuracy', 
                                                  patience=patience)
 
-    # perfrom search for best parameters
+    perfrom search for best parameters
     validation_steps = math.ceil(valid_generators[bodypart].n/ (valid_generators[bodypart].batch_size))
     print("Using validation_steps = %d" % validation_steps)
     steps_per_epoch = math.ceil(train_generators[bodypart].n / (train_generators[bodypart].batch_size))
     print("Using steps_per_epoch = %d" % steps_per_epoch)
+    print("Starting tuner search")
+    
     tuner.search(train_generators[bodypart],
                 validation_data=valid_generators[bodypart],
-                validation_steps = validation_steps,
-                steps_per_epoch = steps_per_epoch,
+                steps_per_epoch=steps_per_epoch,
                 epochs=epochs, 
                 callbacks=[stop_early])
 
     # get the optimal hyperparameters
     best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
-    print('Units: ',best_hps.get('units'), ', Learning rate: ', best_hps.get('learning_rate'), ', Convolutional blocks: ', best_hps.get('conv_blocks'), ', Dropout rate: ', best_hps.get('dropout'))
-    return best_hps
+    print('\nUnits:',best_hps.get('units'), '\nLearning rate:', best_hps.get('learning_rate'), '\nConvolutional blocks:', best_hps.get('conv_blocks'), '\nDropout rate:', best_hps.get('dropout'))    
 
-def build_best_model(best_hps):
-    model = tuner.hypermodel.build(best_hps)
-    return model
+    return tuner, best_hps
 
-def find_best_epoch(model,
-                    monitor,
+
+# find best epoch
+
+def find_best_epoch(best_hps,
                     patience,
-                    epochs):
-    callbacks = tf.keras.callbacks.EarlyStopping(monitor=monitor, 
-                                                patience=patience,
-                                                verbose=1)
+                    bodypart,
+                    epochs,
+                    train_generators,
+                    valid_generators):
+    print(bodypart)
+    print('Build hypermodel')
+    model = tuner.hypermodel.build(best_hps)
+    # define the early stopping
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_binary_accuracy', 
+                                                  patience=patience)
+    # perfrom search for best parameters
     validation_steps = math.ceil(valid_generators[bodypart].n/ (valid_generators[bodypart].batch_size))
     print("Using validation_steps = %d" % validation_steps)
     steps_per_epoch = math.ceil(train_generators[bodypart].n / (train_generators[bodypart].batch_size))
     print("Using steps_per_epoch = %d" % steps_per_epoch)
-    history = model.fit(train_generators[bodypart],
-                        validation_data = valid_generators[bodypart],
-                        validation_steps = validation_steps,
-                        steps_per_epoch = steps_per_epoch,
-                        epochs=epochs,
-                        verbose=1,
-                        callbacks=[callbacks]
-    )
-    
+    print("Fit model")
+    history=model.fit(train_generators[bodypart],
+                        validation_data=valid_generators[bodypart],
+                        steps_per_epoch=steps_per_epoch,
+                        epochs=epochs, 
+                        callbacks=[stop_early])
+
+
     val_acc_per_epoch = history.history['val_accuracy']
+    print("Find best epoch")
     best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
     print('Best epoch: %d' % (best_epoch))
     return best_epoch
 
-def train_best_model(model,
-                    monitor,
-                    patience,
-                    best_epoch):
-    callbacks = tf.keras.callbacks.EarlyStopping(monitor=monitor, 
-                                                patience=patience,
-                                                verbose=1)
-    validation_steps = math.ceil(valid_generators[bodypart].n/ (valid_generators[bodypart].batch_size))
-    print("Using validation_steps = %d" % validation_steps)
-    steps_per_epoch = math.ceil(train_generators[bodypart].n / (train_generators[bodypart].batch_size))
-    print("Using steps_per_epoch = %d" % steps_per_epoch)
-    history = model.fit(train_generators[bodypart],
-                        validation_data = valid_generators[bodypart],
-                        validation_steps = validation_steps,
-                        steps_per_epoch = steps_per_epoch,
-                        epochs=best_epoch,
-                        verbose=1,
-                        callbacks=[callbacks]
-    )
+def build_train_best_model(best_hps,
+                           best_epoch,
+                           patience,
+                           bodypart,
+                           train_generators,
+                           valid_generators):
+    print(bodypart)
+    # instantiate the hypermodel 
+    # train it with the optimal number of epochs
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_binary_accuracy', 
+                                                  patience=patience)
+    hypermodel = tuner.hypermodel.build(best_hps)
+    # plot summary
+    hypermodel.summary()
+    # retrain the model
+    history    = hypermodel.fit(train_generators[bodypart],
+                            validation_data=valid_generators[bodypart],
+                            steps_per_epoch=steps_per_epoch,
+                            epochs=best_epoch, 
+                            callbacks=[stop_early])
     return history
-    
+
+
+#############################
+#############################
